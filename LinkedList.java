d64b648d0e49da6904514483c7a5e4471d91e24e
niranjan310504 <chirdeniranjan31@gmail.com>
2024-07-20 00:00:04 +0530

    created first LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,35 @@
 public class LinkedList {
-    
+    public static class  Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+         
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        //create a new node
+        Node newNode = new Node(data);
+        //check if linked list is empty
+        if(head==null){
+            head = tail = newNode;
+            return;
+        }
+        //assign the value of head to the pointer (newNode.next) of new Node
+        newNode.next = head;
+        //assign head variable the value to new node
+        head = newNode;
+    }
 
 public static void main(String args[]){
-    System.out.println("hello");
+    LinkedList ll = new LinkedList();
+    ll.addFirst(1);
+    ll.addFirst(2);
+    ll.addFirst(3);
 }
 }
